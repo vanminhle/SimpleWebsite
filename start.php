e992ee2085eccd73e653deac8f9cf51aa311d010
vanminh.le <vanminh.le.754@gmail.com>
2021-02-22 16:01:13 +0700

    Update start.php

--- start.php
+++ start.php
@@ -1,8 +1,57 @@
 <html>
-  <head>
-    <title>PHP TEST IN HEROKU</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello World </p>'; ?>
-  </body>
+	<head>
+		<title>PHP Test</title>
+	</head>
+	<body>
+		<?php 
+			echo '<p>TEST HEROKU POSTGRESQL DATABASE </p>'; 
+			# Heroku credential 
+			$host_heroku = "ec2-18-206-84-251.compute-1.amazonaws.com";
+			$db_heroku = "d8k42dnhtd0o9i";
+			$user_heroku = "crmjpgdtqgprga";
+			$pw_heroku = "0d86d0fb5f24be75ffb6728bb2ffaa6762b75489e8923fda3cdf71c519a99d67";
+			# Create connection to Heroku Postgres
+			$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
+			$pg_heroku = pg_connect($conn_string);
+			
+			if (!$pg_heroku)
+			{
+				die('Error: Could not connect: ' . pg_last_error());
+			}
+			# Get data by query
+			$query = 'select * from test_lab6';
+			$result = pg_query($pg_heroku, $query);
+			# Display data column by column
+			$i = 0;
+			echo '<html><body><table><tr>';
+			while ($i < pg_num_fields($result))
+			{
+				$fieldName = pg_field_name($result, $i);
+				echo '<td>' . $fieldName . '</td>';
+				$i = $i + 1;
+			}
+			echo '</tr>';
+			# Display data row by row
+			$i = 0;
+			while ($row = pg_fetch_row($result)) 
+			{
+				echo '<tr>';
+				$count = count($row);
+				$y = 0;
+				while ($y < $count)
+				{
+					$c_row = current($row);
+					echo '<td>' . $c_row . '</td>';
+					next($row);
+					$y = $y + 1;
+				}
+				echo '</tr>';
+				$i = $i + 1;
+			}
+			pg_free_result($result);
+
+			echo '</table></body></html>';
+
+		?> 
+	</body>
 </html>
